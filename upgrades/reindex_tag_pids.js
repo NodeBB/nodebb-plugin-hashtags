fd3b72081f5d85832e99550ed45500a292d9f426
Julian Lam <julian@nodebb.org>
2020-09-23 14:07:31 -0400

    feat: batched upgrade script

--- upgrades/reindex_tag_pids.js
+++ upgrades/reindex_tag_pids.js
@@ -5,12 +5,15 @@ const _ = require('lodash');
 const db = require.main.require('./src/database');
 const topics = require.main.require('./src/topics');
 const posts = require.main.require('./src/posts');
+const batch = require.main.require('./src/batch');
 const main = require('../library');
 
 module.exports = {
 	name: 'Re-index tagged posts list',
 	timestamp: Date.UTC(2020, 8, 22),
-	method: async () => {
+	method: async function () {
+		const progress = this.progress;
+
 		// Take existing tags, index their mainpids
 		const tags = await db.getSortedSetRange('tags:topic:count', 0, -1);
 		let pids = await Promise.all(tags.map(async (tag) => {
@@ -21,11 +24,14 @@ module.exports = {
 		// Join all the arrays, eliminate duplicates
 		pids = pids.reduce((memo, cur) => memo.concat(cur), []).filter(Boolean);
 		pids = _.uniq(pids);
-		console.log('sending', pids, 'in');
 
-		// Turn them pids into pseudo post objects
-		const payload = await posts.getPostsFields(pids, ['pid', 'content']);
+		progress.total = pids.length;
+		await batch.processArray(pids, async (pids) => {
+			progress.incr(pids.length);
 
-		await Promise.allSettled(payload.map(async (post) => main.indexPost({ post })));
+			// Turn them pids into pseudo post objects
+			const payload = await posts.getPostsFields(pids, ['pid', 'content']);
+			await Promise.allSettled(payload.map(async (post) => main.indexPost({ post })));
+		});
 	},
 };
